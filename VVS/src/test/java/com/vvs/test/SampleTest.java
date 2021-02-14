dc57320dd4b0a4ec61f5ad468444e058cdd09fa9
osk14741 <66981983+osk14741@users.noreply.github.com>
2021-02-15 05:21:52 +0900

    asdf

--- VVS/src/test/java/com/vvs/test/SampleTest.java
+++ VVS/src/test/java/com/vvs/test/SampleTest.java
@@ -16,7 +16,7 @@ import com.vvs.shop.TestVO;
 @Transactional
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @WebAppConfiguration
-@RunWith(SpringJUnit4ClassRunner.class) // 스프링 테스트 컨텍스트 프레임워크의 JUnit기능 확장
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/root-context.xml",
 		"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml" })
 public class SampleTest {
